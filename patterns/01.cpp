e52428ec50df878392c26fef6a7029fd07967c3a
user6020Tanishq <tkaushik110036@gmail.com>
2026-05-03 20:29:22 +0530

    updated

---
+++ patterns/01.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+    void pattern(){
+        for(int i = 0; i < 5; i++){
+            std::cout << "*****\n";
+        }
+    }
+     
+};
+
+int main() {
+    Solution a;
+    a.pattern();
+
+}
